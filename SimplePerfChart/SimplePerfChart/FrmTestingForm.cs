bbd53175f16b17c2b5c3601edf7099ec5b73d8a7
cputratama <chris.dp.41@gmail.com>
2017-02-05 19:17:28 +0700

    Update Protocol FrmTestingForm 5-2-2017

--- SimplePerfChart/SimplePerfChart/FrmTestingForm.cs
+++ SimplePerfChart/SimplePerfChart/FrmTestingForm.cs
@@ -18,13 +18,11 @@ using System.IO;
 
 namespace SimplePerfChart
 {
-    
+  
 
-
-public partial class FrmTestingForm : Form
+    public partial class FrmTestingForm : Form
     {
         private object valueGenSync = new object();
-        GMapOverlay markers = new GMapOverlay("markers");
         private Random randGen = new Random();
         //public static decimal data = 0;
         private int valueGenFrom = -5;
@@ -39,9 +37,10 @@ public partial class FrmTestingForm : Form
             public decimal accel_y;
             public decimal accel_z;
         }
-        public FrmTestingForm() {
+        public FrmTestingForm()
+        {
             InitializeComponent();
-            
+
             this.Font = SystemInformation.MenuFont;
 
             propGrid.SelectedObject = perfChart.PerfChartStyle;
@@ -50,13 +49,16 @@ public partial class FrmTestingForm : Form
             perfChart.TimerInterval = 1000;
 
             // Populate DrowDown Boxes
-            foreach (String item in System.Enum.GetNames(typeof(Border3DStyle))) {
+            foreach (String item in System.Enum.GetNames(typeof(Border3DStyle)))
+            {
                 cmbBxBorder.Items.Add(item);
             }
-            foreach (String item in System.Enum.GetNames(typeof(SpPerfChart.ScaleMode))) {
+            foreach (String item in System.Enum.GetNames(typeof(SpPerfChart.ScaleMode)))
+            {
                 cmbBxScaleMode.Items.Add(item);
             }
-            foreach (String item in System.Enum.GetNames(typeof(SpPerfChart.TimerMode))) {
+            foreach (String item in System.Enum.GetNames(typeof(SpPerfChart.TimerMode)))
+            {
                 cmbBxTimerMode.Items.Add(item);
             }
 
@@ -68,33 +70,39 @@ public partial class FrmTestingForm : Form
 
 
 
-        private void chkBxTimerEnabled_CheckedChanged(object sender, EventArgs e) {
-            if (chkBxTimerEnabled.Checked && !bgWrkTimer.IsBusy) {
+        private void chkBxTimerEnabled_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkBxTimerEnabled.Checked && !bgWrkTimer.IsBusy)
+            {
                 RunTimer();
             }
         }
 
-        private void RunTimer() {
+        private void RunTimer()
+        {
             int waitFor = randGen.Next(valueGenTimerFrom, valueGenTimerTo);
             bgWrkTimer.RunWorkerAsync(waitFor);
         }
 
-        private void bgWrkTimer_DoWork(object sender, DoWorkEventArgs e) {
+        private void bgWrkTimer_DoWork(object sender, DoWorkEventArgs e)
+        {
             Thread.Sleep(Convert.ToInt32(e.Argument));
         }
 
-  
+
 
         public class Config
         {
-            
+
             public string host;
             public string user;
             public string password;
-            
+            public string vhost;
+
         }
 
-        private void bgWrkTimer_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e) {
+        private void bgWrkTimer_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
             accel accel_data;
             AccelerationReport accelerations = new AccelerationReport();
             int genValue = randGen.Next(valueGenFrom, valueGenTo);
@@ -104,18 +112,25 @@ public partial class FrmTestingForm : Form
                 string json = r.ReadToEnd();
                 Config config = JsonConvert.DeserializeObject<Config>(json);
 
-                factory = new ConnectionFactory() { HostName = config.host, UserName = config.user, Password = config.password  };
+                factory = new ConnectionFactory() { HostName = config.host, UserName = config.user, VirtualHost = config.vhost, Password = config.password };
+                // factory.Uri = "amqp://lsowqccg:kbLv9YbzjQwxz20NH7Rfy98TTV2eK17j@black-boar.rmq.cloudamqp.com/lsowqccg";
+
             }
 
-            
+
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
-                channel.QueueDeclare(queue: "emergency_gui",
+                channel.QueueDeclare(queue: "ecn", //"emergency_gui",
                                      durable: false,
                                      exclusive: false,
                                      autoDelete: false,
                                      arguments: null);
+                channel.QueueBind(queue: "ecn", //"emergency_gui",
+                                     exchange: "amq.topic",
+                                     routingKey: "amq.topic.ecn" //emergency"
+                                     );
+
                 Console.WriteLine("Queue Declare Emergency GUI");
                 var consumer = new EventingBasicConsumer(channel);
                 consumer.Received += (model, ea) =>
@@ -130,77 +145,83 @@ public partial class FrmTestingForm : Form
                         AccelerationReport accelReport = JsonConvert.DeserializeObject<AccelerationReport>(message);
 
                         //Console.WriteLine("{0} {1} {2}", accel_data.accel_x, accel_data.accel_y, accel_data.accel_z);
-                        Console.WriteLine("{0} {1} {2} {3}", accelReport.geometry.geometry.coordinates, accelReport.accelerations[0].x, accelReport.accelerations[0].y, accelReport.accelerations[0].z);
-                        perfChart.AddValue(Math.Abs((decimal)accelReport.accelerations[0].x * 1000));
-                        perfChart1.AddValue(Math.Abs((decimal)accelReport.accelerations[0].y * 1000));
-                        perfChart2.AddValue(Math.Abs((decimal)accelReport.accelerations[0].z * 1000));
-                        markers.Markers.Clear();
-                        GMapMarker marker1 = new GMarkerGoogle(
-                        new PointLatLng(-6.890910, 107.610300),
-                        GMarkerGoogleType.blue_pushpin);
-                        markers.Markers.Add(marker1);
-                        gMapControl1.Overlays.Add(markers);
-                    } catch (Exception ex)
+                        //console.writeline("{0} {1} {2} {3}", accelreport.geometry.geometry.coordinates, accelreport.accelerations[0].x, accelreport.accelerations[0].y, accelreport.accelerations[0].z);
+                        //perfchart.addvalue((decimal)accelreport.accelerations[0].x * 1000);
+                        //perfchart1.addvalue((decimal)accelreport.accelerations[0].y * 1000);
+                        //perfchart2.addvalue((decimal)accelreport.accelerations[0].z * 1000);
+                        Console.WriteLine("{0} {1} {2} {3} {4}", accelReport.geometry.geometryData.coordinate.lon, accelReport.geometry.geometryData.coordinate.lng, accelReport.accelerations.x, accelReport.accelerations.y, accelReport.accelerations.z);
+                        perfChart.AddValue((decimal)accelReport.accelerations.x * 1000);
+                        perfChart1.AddValue((decimal)accelReport.accelerations.y * 1000);
+                        perfChart2.AddValue((decimal)accelReport.accelerations.z * 1000);
+                    }
+                    catch (Exception ex)
                     {
                         Console.WriteLine("ERROR: {0}", ex);
                     }
 
                 };
-                channel.BasicConsume(queue: "emergency_gui",
+                channel.BasicConsume(queue: "ecn", //"emergency_gui",
                                      noAck: true,
                                      consumer: consumer);
 
                 Console.WriteLine("Already BasicConsume");
                 //Console.ReadLine();
             }
-            
-            if (chkBxTimerEnabled.Checked) {
+
+            if (chkBxTimerEnabled.Checked)
+            {
                 //Simply restart, if still enabled
                 RunTimer();
             }
         }
 
-        private void cmbBxBorder_SelectedIndexChanged(object sender, EventArgs e) {
+        private void cmbBxBorder_SelectedIndexChanged(object sender, EventArgs e)
+        {
             perfChart.BorderStyle = (Border3DStyle)Enum.Parse(
                 typeof(Border3DStyle), cmbBxBorder.SelectedItem.ToString()
             );
         }
 
-        private void cmbBxScaleMode_SelectedIndexChanged(object sender, EventArgs e) {
+        private void cmbBxScaleMode_SelectedIndexChanged(object sender, EventArgs e)
+        {
             perfChart.ScaleMode = (SpPerfChart.ScaleMode)Enum.Parse(
                 typeof(SpPerfChart.ScaleMode), cmbBxScaleMode.SelectedItem.ToString()
             );
         }
 
-        private void cmbBxTimerMode_SelectedIndexChanged(object sender, EventArgs e) {
+        private void cmbBxTimerMode_SelectedIndexChanged(object sender, EventArgs e)
+        {
             perfChart.TimerMode = (SpPerfChart.TimerMode)Enum.Parse(
                 typeof(SpPerfChart.TimerMode), cmbBxTimerMode.SelectedItem.ToString()
             );
         }
 
-        private void numUpDnTimerInterval_ValueChanged(object sender, EventArgs e) {
+        private void numUpDnTimerInterval_ValueChanged(object sender, EventArgs e)
+        {
             perfChart.TimerInterval = Convert.ToInt32(numUpDnTimerInterval.Value);
         }
 
-        private void btnApply_Click(object sender, EventArgs e) {
+        private void btnApply_Click(object sender, EventArgs e)
+        {
             valueGenFrom = Convert.ToInt32(numUpDnValFrom.Value);
             valueGenTo = Convert.ToInt32(numUpDnValTo.Value);
-            if (valueGenTo < valueGenFrom) {
+            if (valueGenTo < valueGenFrom)
+            {
                 valueGenTo = valueGenFrom;
                 numUpDnValTo.Value = valueGenTo;
             }
 
             valueGenTimerFrom = Convert.ToInt32(numUpDnFromInterval.Value);
             valueGenTimerTo = Convert.ToInt32(numUpDnToInterval.Value);
-            if (valueGenTimerTo < valueGenTimerFrom) {
+            if (valueGenTimerTo < valueGenTimerFrom)
+            {
                 valueGenTimerTo = valueGenTimerFrom;
                 numUpDnToInterval.Value = valueGenTimerTo;
             }
-
-            
         }
 
-        private void btnClear_Click(object sender, EventArgs e) {
+        private void btnClear_Click(object sender, EventArgs e)
+        {
             perfChart.Clear();
         }
 
@@ -245,12 +266,12 @@ public partial class FrmTestingForm : Form
 
         private void gMapControl1_Load(object sender, EventArgs e)
         {
-           
+
             gMapControl1.MapProvider = GMap.NET.MapProviders.BingMapProvider.Instance;
             GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
             gMapControl1.Position = new GMap.NET.PointLatLng(-6.9175, 107.6191);
             gMapControl1.ShowCenter = false;
-            
+            GMapOverlay markers = new GMapOverlay("markers");
             GMapMarker marker = new GMarkerGoogle(
                 new PointLatLng(-6.890903, 107.610378),
                 GMarkerGoogleType.blue_pushpin);
@@ -281,26 +302,40 @@ public partial class FrmTestingForm : Form
         }
     }
 
+
     class AccelerationReport
     {
         public string pointTime;
         public string timeZone;
-        public Geometry geometry;
-        public AccelerationSample[] accelerations;
+        public string interval;
+        public geojson geometry;
+        public AccelerationSample accelerations;
 
     }
 
 
-    class Geometry
+    class geojson
     {
         public string type;
-        public GeometryData geometry;
+        public geometry geometryData;
+        public properties property;
     }
 
-    class GeometryData
+    class geometry
     {
         public string type;
-        public float[] coordinates;
+        public coordinates coordinate;
+    }
+
+    class properties
+    {
+        public string name;
+    }
+
+    class coordinates
+    {
+        public string lng;
+        public string lon;
     }
 
     class AccelerationSample
